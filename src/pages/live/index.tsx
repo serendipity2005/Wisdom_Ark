b7fe73f831fccecfbcabb4895f7ed7c6dd25f58d
hann <2767073256@qq.com>
2025-08-02 15:24:34 +0800

    feat:直播视频展示部分

--- src/pages/live/index.tsx
+++ src/pages/live/index.tsx
@@ -1,10 +1,16 @@
 import MyHeader from '@/layouts/frontLayout/Header';
 import React from 'react';
+import VideoShow from '@/components/Live/VideoShow';
+import VideoList from '@/components/Live/VideoList';
 
 export default function Live() {
   return (
     <>
       <MyHeader></MyHeader>
+      <div className="w-100% h-620 bg-#232323 flex justify-center items-center">
+        <VideoShow></VideoShow>
+        <VideoList></VideoList>
+      </div>
     </>
   );
 }
